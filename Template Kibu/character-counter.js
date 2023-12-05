95ca05450d6951ba277ca583be01dced40e49d92
IthielGit <ithiel.ramaroharinosy@brainsolutions.be>
2023-12-05 17:20:20 +0300

    init project

---
+++ Template Kibu/character-counter.js
@@ -0,0 +1,21 @@
+
+  // Wait for the DOM to be fully loaded
+  document.addEventListener("DOMContentLoaded", function() {
+    // Get the textarea element
+    var textArea = document.getElementsByName("MultiLine")[0];
+
+    // Add an event listener for 'input' event on the textarea
+    textArea.addEventListener('input', updateCharacterCount);
+
+    // The updateCharacterCount function
+    function updateCharacterCount() {
+      var charCount = textArea.value.length;
+      var maxChar = textArea.getAttribute("maxlength");
+      // Update the paragraph content
+      document.getElementById("characterCount").textContent = charCount + " of " + maxChar + " max characters";
+    }
+
+    // Initialize character count on load
+    updateCharacterCount();
+  });
+
